5e74dc9ebb68caea530eda0265a657c25b8153cf
Emre YİĞİT <emreygt67@icloud.com>
2021-03-20 12:35:40 +0300

    Proje dosyası ekle.

---
+++ switchcase/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kaçıncı aydasınız: ");
+            string ay = Convert.ToString(Console.ReadLine());
+
+            switch(ay)
+            {
+                case "a":
+                    Console.WriteLine("ocak");
+                    break;
+                case "b":
+                    Console.WriteLine("şubat");
+                    break;
+                case "c":
+                    Console.WriteLine("mart");
+                    break;
+                case "4":
+                    Console.WriteLine("nisan");
+                    break;
+                case "5":
+                    Console.WriteLine("mayıs");
+                    break;
+                case "6":
+                    Console.WriteLine("haziran");
+                    break;
+                case "7":
+                    Console.WriteLine("temmuz");
+                    break;
+                case "8":
+                    Console.WriteLine("ağustos");
+                    break;
+                   
+                case "9":
+                    Console.WriteLine("eylül");
+                    break;
+                case "10":
+                    Console.WriteLine("ekim");
+                    break;
+                case "11":
+                    Console.WriteLine("kasım");
+                    break;
+                case "12":
+                    Console.WriteLine("aralık");
+                    break;
+                
+                    
+            }
+                
+
+
+
+            
+           
+               
+
+
+        }
+    }
+}
